08b48cc43f8dd9dcecf1ae70595dd3c82736dcbd
David <mr.hilleke@gmail.com>
2022-12-03 15:07:27 -0500

    work from saturday class

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,14 +3,12 @@ import Navigation from './Components/Navigation';
 import PostBox from './Components/Postbox';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import map from './map.webp';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-     <Navigation/>
-      {/* <img src={map} className="BG-img" alt="bg-img"/> */}
+      <Navigation/>
       <PostBox/>
     </div>
   );
