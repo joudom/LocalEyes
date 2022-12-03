f03a4e52bc62078a60a24a1072437b1179bc7984
ejcGH <iam3jc@gmail.com>
2022-12-03 11:47:44 -0500

    react routers to home and register page

--- Client/src/Components/Register.js
+++ Client/src/Components/Register.js
@@ -2,9 +2,12 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Navigation from './Navigation';
 
 function Register() {
   return (
+    <>
+   <Navigation/>
     <Form>
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridEmail">
@@ -55,7 +58,8 @@ function Register() {
       <Button variant="primary" type="submit">
         Submit
       </Button>
-    </Form>
+      </Form>
+      </>
   );
 }
 
--- Client/src/index.js
+++ Client/src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import background from './map.webp';
+import { BrowserRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <div className="background" style={{ backgroundImage: `url(${background})` }}>
       <App />
     </div>
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
