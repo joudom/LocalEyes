2cecddec21d0e52acf400218931fab0aa4bc2a3a
Jesse Oudom <jesse.oudom@yahoo.com>
2022-12-06 19:44:30 -0500

    deletes boilerplate imgs and original logo img

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,5 @@
-
-import Navigation from './Components/Navigation';
-import PostBox from './Components/Postbox';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import map from './map.webp';
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Home from './Components/Home';
