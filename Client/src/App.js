b6932f10ac4d1c0491cbb94350a93dd1a6767bf4
ejcGH <iam3jc@gmail.com>
2022-12-06 21:39:25 -0500

    set up of backend server for file upload

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,21 +1,23 @@
-
-import Navigation from './Components/Navigation';
-import PostBox from './Components/Postbox';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import map from './map.webp';
-import './App.css';
-import { Routes, Route } from "react-router-dom"
-import Home from './Components/Home';
-import Register from './Components/Register';
+import Navigation from "./Components/Navigation";
+import PostBox from "./Components/Postbox";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import map from "./map.webp";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Register from "./Components/Register";
+import PostPage from "./Components/PostPage";
+import Upload from "./Components/Upload";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='register' element={ <Register/>}/>
-    </Routes>
+        <Route path="register" element={<Register />} />
+        <Route path="upload" element={<Upload />} />
+      </Routes>
     </div>
   );
 }
