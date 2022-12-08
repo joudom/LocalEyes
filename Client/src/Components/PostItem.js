3a8ba4400126b51544f169fef0e6016137e0f693
Jesse Oudom <jesse.oudom@yahoo.com>
2022-12-08 00:10:42 -0500

    adds opacity and some responsiveness to PostItem

--- Client/src/Components/PostItem.js
+++ Client/src/Components/PostItem.js
@@ -12,7 +12,7 @@ const PostItem = () => {
     <Navigation />
       <Container className="postbox">
         <Row className="h-50">
-          <Col sm={8} className='border'>
+          <Col sm={8} className='text border'>
             Image Carousel<br/>
             <img src
             ="https://media.istockphoto.com/id/1202959798/photo/macbook-pro-16-inch-with-touchbar-front-view.jpg
@@ -24,33 +24,33 @@ const PostItem = () => {
                   <h1>$750.00</h1>
                 Posted: 12/06/2022<br/>
                 Ends: 12/09/2022<br/><br/>
-              <div className="location">
+              <div className="text location">
                 STORE: Best Buy<br/>
                 ADDRESS: 1234 Shopping Blvd Atlanta, GA 30313
               </div>
                 <br/>
               <div>
                 Is this still on sale?<br/><br/>
-                <button>YES</button>  <button>NO</button>
+                <button>YES</button> <button>NO</button>
               </div>
             </div>
           </Col>
         </Row>
           <br/>
-        <Row className="h-50">
-          <Col sm className='border'>
+        <Row style={{height: "39vh"}}>
+          <Col sm className='text border'>
             Item Description<br/><br/>
             The new MacBook Pro delivers game-changing performance for pro users. 
             With the powerful M1 Pro to supercharge pro-level workflows while getting amazing battery life.
             And with an immersive 14-inch Liquid Retina XDR display and an array of pro ports, you can do more than ever with MacBook Pro.
           </Col>
-          <Col sm className='border'>
+          <Col sm className='text border'>
             Local Deal Description<br/><br/>
             They have brand new MacBooks for the low out here!
             Manager says they have to sell everything to close Q4 on a strong note.
           </Col>
-          <Col sm className='border'>
-            Local: <br/><br/>
+          <Col sm className='text border'>
+            Local:<br/>
             Leo D.<br/>
             <img src="https://images.t3n.de/news/wp-content/uploads/2022/05/Leonardo-DiCaprio-Meme.png?class=structuredData-small" className='local' alt="..."/>
           </Col>
