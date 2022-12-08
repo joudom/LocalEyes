8ae42906c26519d1907eebe375bc16462c0b3d7a
Jesse Oudom <jesse.oudom@yahoo.com>
2022-12-07 21:38:42 -0500

    Updated with E  ad J's codes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,20 +1,23 @@
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Register from "./Components/Register";
 import Upload from "./Components/Upload";
+import PostItem from './Components/PostItem';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="register" element={<Register />} />
-        <Route path="upload" element={<Upload />} />
+        <Route path="/"         element={<Home/>}     />
+        <Route path="register"  element={<Register/>} />
+        <Route path="upload"    element={<Upload/>}   />
+        <Route path='item'      element={<PostItem/>} />
       </Routes>
+      <Footer/> 
     </div>
   );
 }
