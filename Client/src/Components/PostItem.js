b802f37ffe7cf7e68aa0f12b2fb782d48ae79a8e
Jesse Oudom <jesse.oudom@yahoo.com>
2022-12-08 19:05:32 -0500

    adds styling to PostItem component

--- Client/src/Components/PostItem.js
+++ Client/src/Components/PostItem.js
@@ -12,13 +12,13 @@ const PostItem = () => {
     <Navigation />
       <Container className="postbox">
         <Row className="h-50">
-          <Col sm={8} className='text border'>
+          <Col xxl={8} className='text border'>
             Image Carousel<br/>
             <img src
             ="https://media.istockphoto.com/id/1202959798/photo/macbook-pro-16-inch-with-touchbar-front-view.jpg
               ?s=612x612&w=0&k=20&c=Uj7nnv5j_STbkW03MaXNKQtdUxiN5AQD9JF0Dw1i0WQ="className="img" alt="..."/>
           </Col>
-          <Col sm={4} className='border'>
+          <Col xxl={4} className='border'>
             <div className="text">
                 Pricing / Location / Status<br/>
                   <h1>$750.00</h1>
@@ -37,19 +37,19 @@ const PostItem = () => {
           </Col>
         </Row>
           <br/>
-        <Row style={{height: "39vh"}}>
-          <Col sm className='text border'>
+        <Row style={{height: "39.2vh"}}>
+          <Col xxl className='text border'>
             Item Description<br/><br/>
             The new MacBook Pro delivers game-changing performance for pro users. 
             With the powerful M1 Pro to supercharge pro-level workflows while getting amazing battery life.
             And with an immersive 14-inch Liquid Retina XDR display and an array of pro ports, you can do more than ever with MacBook Pro.
           </Col>
-          <Col sm className='text border'>
+          <Col xxl className='text border'>
             Local Deal Description<br/><br/>
             They have brand new MacBooks for the low out here!
             Manager says they have to sell everything to close Q4 on a strong note.
           </Col>
-          <Col sm className='text border'>
+          <Col xxl className='text border'>
             Local:<br/>
             Leo D.<br/>
             <img src="https://images.t3n.de/news/wp-content/uploads/2022/05/Leonardo-DiCaprio-Meme.png?class=structuredData-small" className='local' alt="..."/>
