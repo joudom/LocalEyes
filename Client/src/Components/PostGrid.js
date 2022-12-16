f02b4a755b6ae6994c72acb88f5a8a4d1e61afe7
Jesse Oudom <jesse.oudom@yahoo.com>
2022-12-15 21:19:28 -0500

    cosmetic housecleaning

--- Client/src/Components/PostGrid.js
+++ Client/src/Components/PostGrid.js
@@ -1,11 +1,9 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Container from 'react-bootstrap/Container';
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import './PostGrid.css';
-
 
 function PostGrid({posts, setItem}) {
 
